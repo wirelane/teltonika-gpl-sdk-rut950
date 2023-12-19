1db7c1f782f65cdcd93a90a32ed6692957a044d0
Curd Becker <me@curd-becker.de>
2023-12-20 00:00:00 +0100

    SDK RUT9_R_GPL_00.07.06

--- target/linux/ath79/image/24kc.mk
+++ target/linux/ath79/image/24kc.mk
@@ -1,6 +1,7 @@
 #
 # MIPS 24Kc Devices
 #
+include $(INCLUDE_DIR)/hardware.mk
 
 define Device/qca9531
 	SOC := qca9531
@@ -14,10 +15,10 @@ define Device/teltonika_rut2xx
 	$(Device/LegacyRUT)
 	$(Device/ar9330)
 	DEVICE_MODEL := RUT2XX
-	DEVICE_FEATURES += io wifi verified_boot high-watchdog-priority 64mb_ram
+	DEVICE_BOOT_NAME := tlt-rut2xx tlt-rut2xx-vboot tlt-rut2xx-vboot-64k
+	DEVICE_FEATURES += io wifi verified_boot high-watchdog-priority 64mb_ram mobile
 	DEVICE_COMPAT_CODE := "RUT240.*"
 	TLT_LEGACY_MODEL := RUT2
-	UBOOT_NAME := tlt-rut2xx
 	TPLINK_HWID := 0x32200002
 
 	# Default common packages for RUT2XX series
@@ -26,16 +27,24 @@ define Device/teltonika_rut2xx
 	DEVICE_PACKAGES += kmod-usb-chipidea2
 
 	# Wireless related:
-	DEVICE_PACKAGES += kmod-ath9k
+	DEVICE_PACKAGES += kmod-ath9k_54
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	# Static board information
+	HARDWARE/Ethernet/Port := 2
+	HARDWARE/Ethernet/Speed := $(HW_ETH_SPEED_100)
+	HARDWARE/Ethernet/Standard := $(HW_ETH_STD_80211) $(HW_ETH_STD_80211U) $(HW_ETH_STD_AUTO_MDI_MDIX)
+	HARDWARE/Ethernet/WAN/Port := 1
+	HARDWARE/Ethernet/LAN/Port := 1
 endef
 TARGET_DEVICES += teltonika_rut2xx
 
 define Device/teltonika_trb2xx
 	$(Device/qca9531)
 	DEVICE_MODEL := TRB2XX
-	DEVICE_FEATURES += gateway gps serial serial-reset-quirk modbus io single-port dualsim bacnet ntrip 64mb_ram
-	UBOOT_NAME := tlt-trb24x
+	DEVICE_BOOT_NAME := tlt-trb24x
+	DEVICE_FEATURES += gateway gps serial serial-reset-quirk modbus io \
+			single-port dualsim bacnet ntrip mobile 64mb_ram
 	NO_ART := 1
 	# Default common packages for TRB2XX series
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -49,30 +58,11 @@ define Device/teltonika_trb2xx
 endef
 TARGET_DEVICES += teltonika_trb2xx
 
-define Device/teltonika_otd1xx
-	$(Device/qca9531)
-	DEVICE_MODEL := OTD1XX
-	DEVICE_FEATURES += wifi dualsim
-	UBOOT_NAME := tlt-otd1xx
-	# Default common packages for OTD1XX series
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	# Essential must-have:
-	DEVICE_PACKAGES := kmod-spi-gpio
-
-	# USB related:
-	DEVICE_PACKAGES += kmod-usb2
-
-	# Wireless related:
-	DEVICE_PACKAGES += kmod-ath9k kmod-ath10k ath10k-firmware-qca9887
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-endef
-TARGET_DEVICES += teltonika_otd1xx
-
 define Device/teltonika_rut30x
 	$(Device/qca9531)
 	DEVICE_MODEL := RUT30X
-	DEVICE_FEATURES += usb-port serial io port-mirror basic-router 64mb_ram
-	UBOOT_NAME := tlt-rut300
+	DEVICE_BOOT_NAME := tlt-rut300
+	DEVICE_FEATURES += usb-port serial io port-mirror basic-router 64mb_ram ledman-lite
 	NO_ART := 1
 	# Default common packages for RUT30X series
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -85,8 +75,8 @@ TARGET_DEVICES += teltonika_rut30x
 define Device/teltonika_rut36x
 	$(Device/qca9531)
 	DEVICE_MODEL := RUT36X
-	DEVICE_FEATURES += io wifi
-	UBOOT_NAME := tlt-rut360
+	DEVICE_BOOT_NAME := tlt-rut360
+	DEVICE_FEATURES += io wifi mobile
 	# Default common packages for RUT36X series
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	# Essential must-have:
@@ -96,7 +86,7 @@ define Device/teltonika_rut36x
 	DEVICE_PACKAGES += kmod-usb2
 
 	# Wireless related:
-	DEVICE_PACKAGES += kmod-ath9k
+	DEVICE_PACKAGES += kmod-ath9k_54
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 endef
 TARGET_DEVICES += teltonika_rut36x
@@ -104,8 +94,8 @@ TARGET_DEVICES += teltonika_rut36x
 define Device/teltonika_tcr1xx
 	$(Device/qca9531)
 	DEVICE_MODEL := TCR1XX
-	DEVICE_FEATURES += wifi guest-wifi rfkill wps
-	UBOOT_NAME := tlt-tcr1xx
+	DEVICE_BOOT_NAME := tlt-tcr1xx
+	DEVICE_FEATURES += wifi guest-wifi rfkill wps mobile
 	# Default common packages for TCR1XX series
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	# Essential must-have:
@@ -115,7 +105,7 @@ define Device/teltonika_tcr1xx
 	DEVICE_PACKAGES += kmod-usb2
 
 	# Wireless related:
-	DEVICE_PACKAGES += kmod-ath9k kmod-ath10k ath10k-firmware-qca9887
+	DEVICE_PACKAGES += kmod-ath9k_54 kmod-ath10k_54 ath10k-firmware-qca9887
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 endef
 TARGET_DEVICES += teltonika_tcr1xx
--- target/linux/ath79/image/74kc.mk
+++ target/linux/ath79/image/74kc.mk
@@ -10,22 +10,23 @@ define Device/teltonika_rut9xx
 	$(Device/LegacyRUT)
 	$(Device/ar9344)
 	DEVICE_MODEL := RUT9XX
-	DEVICE_FEATURES += usb-port gps serial serial-reset-quirk modbus port-mirror io wifi dualsim verified_boot bacnet ntrip
+	DEVICE_BOOT_NAME := tlt-rut9xx tlt-rut9xx-vboot tlt-rut9xx-vboot-64k
+	DEVICE_FEATURES += usb-port gps serial serial-reset-quirk modbus \
+			port-mirror io wifi dualsim verified_boot bacnet ntrip mobile
 	TLT_LEGACY_MODEL := RUT9
-	UBOOT_NAME := tlt-rut9xx
 	TPLINK_HWID := 0x35000001
 
 	# Default common packages for RUT9XX series
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	# Essential must-have:
 	DEVICE_PACKAGES := kmod-spi-gpio kmod-mmc-spi kmod-i2c-gpio \
-			   kmod-hwmon-mcp3021 kmod-hwmon-tla2021 kmod-cypress-serial
+			kmod-hwmon-mcp3021 kmod-hwmon-tla2021 kmod-cypress-serial
 
 	# USB related:
 	DEVICE_PACKAGES += kmod-usb2
 
 	# Wireless related:
-	DEVICE_PACKAGES += kmod-ath9k
+	DEVICE_PACKAGES += kmod-ath9k_54
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 endef
 TARGET_DEVICES += teltonika_rut9xx
